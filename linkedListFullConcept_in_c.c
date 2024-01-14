bbf29e356f939351f8b08ccf607a7651c2ab03a7
dharmendra016 <dharmendra.078bct016@tcioe.edu.np>
2024-01-14 20:22:50 +0545

    adding other methods

--- linkedListFullConcept_in_c.c
+++ linkedListFullConcept_in_c.c
@@ -14,6 +14,7 @@ void insertAtFirst(struct Node** head  , int data){
     temp->val = data ; 
 
     if( *head == NULL ){
+        printf("The linkelist is empty\n");
         temp->next = NULL ;
         *head = temp; 
         return ;
@@ -47,6 +48,72 @@ void deleteLastNode(struct Node** head){
 
 }
 
+void searchNode(struct Node** head  , int data){
+
+    struct Node* temp = *head ; 
+    int cnt = 1 ;
+
+    while(temp != NULL){
+        if( temp->val == data){
+            printf("Founded node at position %d" , cnt );
+        }
+        temp= temp->next ; 
+        cnt++;
+    }
+
+}
+
+void insertAtLast(struct Node** head  , int data){
+    struct Node* curr = *head ;
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node)); 
+    temp->val = data ; 
+
+    if( *head == NULL ){
+        temp->next = NULL ;
+        *head = temp; 
+        return ;
+    }
+    while(curr->next != NULL ){
+        curr = curr->next ; 
+    }
+    temp->next = NULL ; 
+    curr->next = temp ; 
+}
+
+void insertAtMiddle(struct Node** head  , int position , int data){
+    
+    if( position == 1 ){
+        insertAtFirst(head , data);
+        return ;
+    }
+
+    int cnt = 1 ; 
+    struct Node* temp = *head ;
+    while(temp->next != NULL){
+        temp = temp->next ; 
+        cnt++ ; 
+    }
+
+    if(position == cnt){
+        insertAtLast(head , data);
+        return;
+    }
+    cnt = 1; 
+    temp = *head ; 
+
+    while((cnt+1) != position){
+        temp = temp->next ; 
+        cnt++ ; 
+    }
+
+    struct Node* new = (struct Node*)malloc(sizeof(struct Node)); 
+    new->val = data ; 
+    
+    new->next = temp->next ; 
+    temp->next = new ;  
+
+}
+
 int main(){
     struct Node* head = (struct Node*)malloc(sizeof(struct Node));   
     head->val = 12 ; 
@@ -63,9 +130,13 @@ int main(){
     printAll(head);
     deleteLastNode(&head) ; 
     printAll(head);
-
-
-
+    searchNode(&head , 45);
+    insertAtLast(&head , 99);
+    printAll(head);
+    insertAtMiddle(&head , 3,80);
+    printAll(head) ; 
+    insertAtMiddle(&head , 1,10);
+    printAll(head) ; 
 
     return 0;
 }
